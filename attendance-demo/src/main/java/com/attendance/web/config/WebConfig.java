8da95155d7762676d96e5c75e602fb20fe505cdd
jiangwen19 <jiangwen9@yahoo.com>
2022-01-13 02:35:29 +0900

    async

--- attendance-demo/src/main/java/com/attendance/web/config/WebConfig.java
+++ attendance-demo/src/main/java/com/attendance/web/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -19,11 +20,18 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     private TimeInterceptor timeInterceptor;
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(timeInterceptor);
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+//        configurer.registerCallableInterceptors();
+//        configurer.setDefaultTimeout();
+//        configurer.setTaskExecutor();
     }
 
-    @Bean
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(timeInterceptor);
+//    }
+
+    //    @Bean
     public FilterRegistrationBean timeFilter() {
         FilterRegistrationBean registrationBean = new FilterRegistrationBean();
         TimeFilter timeFilter = new TimeFilter();
