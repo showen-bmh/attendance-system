fe4b20445057376ee6e41ba19a78c1f2b084dbb0
jiangwen19 <jiangwen9@yahoo.com>
2022-01-17 03:06:35 +0900

    userdtail injection not resolved

--- attendance/attendance-security/src/main/java/com/attendance/security/browser/MyUserDetailService.java
+++ attendance/attendance-security/src/main/java/com/attendance/security/browser/MyUserDetailService.java
@@ -1,21 +1,15 @@
 package com.attendance.security.browser;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class MyUserDetailService implements UserDetailsService {
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    private String password = passwordEncoder.encode("123456");
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -24,6 +18,7 @@ public class MyUserDetailService implements UserDetailsService {
 //            throw new UsernameNotFoundException("用户名或密码不存在");
 //        }
 //        return new AccountUser(userInfo.getUserId(), userInfo.getUsername(), userInfo.getPassword(), getUserAuthority(userInfo.getUserId()));
-        return new User("admin", password, true, true, true, false, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+
+        return new User("hello", new BCryptPasswordEncoder().encode("123"), AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
     }
 }
