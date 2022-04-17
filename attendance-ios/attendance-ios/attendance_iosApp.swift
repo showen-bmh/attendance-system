bd5b2fad8b2ef2c9d821fa3e3a3e23f3e80e6725
jiangwen <jiangwen9@yahoo.com>
2022-04-17 22:05:46 +0900

    find

---
+++ attendance-ios/attendance-ios/attendance_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  attendance_iosApp.swift
+//  attendance-ios
+//
+//  Created by 蒋穏 on 2022/04/17.
+//
+
+import SwiftUI
+
+@main
+struct attendance_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
